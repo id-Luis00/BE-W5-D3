9a65a3aa33f89744afe7da3d42e8a000174a172c
ed-Luis00 <edinsonluis2014@gmail.com>
2024-09-11 16:24:18 +0200

    first commit

---
+++ src/main/java/luisQuiroz/BE_W5_D3/BeW5D3Application.java
@@ -0,0 +1,13 @@
+package luisQuiroz.BE_W5_D3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeW5D3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeW5D3Application.class, args);
+	}
+
+}
